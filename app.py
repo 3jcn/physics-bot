c3620fae2c53626c89c04b316443dcda4231f706
3jcn <31360292+3jcn@users.noreply.github.com>
2021-04-05 19:54:42 -0700

    Update app.py

--- app.py
+++ app.py
@@ -290,3 +290,57 @@ def run_query(input):
             outp=wikipedia.summary(input,sentences=1)
             talk(outp)
 
+#################################################################################
+header = st.beta_container()
+
+st.markdown("""
+	<style>
+	.main{
+	background-color: #f5f5f5;
+	}
+        .stButton>button {
+        background-color: #0000ff;
+        color: #ffffff;
+        }
+        .stTextInput>div>div>input {
+        background-color: yellow;
+        color: brown;
+        }
+	</style>
+	""",
+	unsafe_allow_html=True
+)
+
+with header:
+    st.title('Virtual Assistant for Introductory of Physical Science - PSC 110')
+    st.text('@Author: Thomas Nguyen Date: 28 Feb 2021')
+
+    image = Image.open('max.png')
+    st.image(image)
+
+    html_temp = """
+	<div style="background-color:brown; padding:10px">
+	<h2 style="color:white; text-align:center;">My name is Max. I am professor Nguyen's assistant.</h2>
+	</div>
+	"""
+    st.markdown(html_temp,unsafe_allow_html=True)
+    
+    st.text('Hi Students, ask me some questions about physical science!')
+    st.text("For example: 'what is temperature, specific heat, entropy, first law of thermodynamics,... ") 
+    st.text("If you are tired, you can ask me to play music by saying: play 'song name or artist name'")
+    st.text("or 'who is ..?', 'what time is it?', 'what date is it?'...")
+    
+    user_input = st.text_input("Enter your question here OR click 'Chat With Max' button:")
+    if user_input:
+        if 'no' == user_input:
+            talk('on behalf of professor Nguyen, thank you for studying. bye.')
+        else:
+            run_query(user_input)
+    st.text("Say 'No' to stop the conversation or click 'Stop' button on the top right corner.")
+
+
+    button_start = st.button('Chat With Max')
+    if button_start:
+    	start_function()
+
+############################################################################      
