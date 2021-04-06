78881be360dedfce787881b1145ad41073424729
3jcn <31360292+3jcn@users.noreply.github.com>
2021-04-05 22:01:57 -0700

    Update app.py

--- app.py
+++ app.py
@@ -16,8 +16,7 @@ def talk(text):
       speech = gTTS(text, lang = 'en', slow = False)
       speech.save('trans.mp3')                          
       audio_file = open('trans.mp3', 'rb')   
-      audio_bytes = audio_file.read()         
-      #playsound(audio_bytes)      
+      audio_bytes = audio_file.read()  
       st.audio(audio_bytes, format='audio/ogg',start_time=0)
       os.remove("trans.mp3")
 	
